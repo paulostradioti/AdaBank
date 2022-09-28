50eed47aa0d374e803f751d406e7d0e4c06c7a3e
Paulo Ricardo Stradioti <paulo.stradioti@ibm.com>
2022-09-28 17:28:45 -0300

    Adicionando Testes de Integração

---
+++ AdaBank.IntegrationTests/Models/WeatherForecastGetRequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AdaBank.IntegrationTests.Models
+{
+    public class WeatherForecastGetRequestModel
+    {
+        public string? Date { get; set; }
+
+        public string? TemperatureC { get; set; }
+
+        public string? TemperatureF { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
