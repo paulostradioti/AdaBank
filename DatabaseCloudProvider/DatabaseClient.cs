e7f8a522a1ce2838d100631e1d9835e0daa4549a
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2022-09-30 18:19:58 -0300

    Adicionando Dependencia Fake

---
+++ DatabaseCloudProvider/DatabaseClient.cs
@@ -0,0 +1,31 @@
+﻿namespace DatabaseCloudProvider
+{
+    public class DatabaseClient<T> : IDatabaseClient<T>
+    {
+        readonly Random _random = Random.Shared;
+        private readonly Dictionary<string, T> _dbSet = new();
+
+        public async Task InsertAsync(string id, T item)
+        {
+            await Task.Delay(_random.Next(100,200));
+
+            _dbSet.Add(id, item);
+        }
+
+        public async Task<IReadOnlyCollection<T>> ScanAsync()
+        {
+            await Task.Delay(_random.Next(100, 200));
+
+            return _dbSet.Values;
+        }
+
+        public async Task<T> GetAsync(string id)
+        {
+            await Task.Delay(_random.Next(100, 200));
+
+            var contains = _dbSet.TryGetValue(id, out var item);
+
+            return  contains ? item : default;
+        }
+    }
+}
---
+++ DatabaseCloudProvider/IDatabaseClient.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabaseCloudProvider
+{
+    public interface IDatabaseClient<T>
+    {
+        Task<T> GetAsync(string id);
+        Task<IReadOnlyCollection<T>> ScanAsync();
+        Task InsertAsync(string id, T value);
+    }
+}
