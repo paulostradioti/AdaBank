de154a49bc8a04069e922c469922d5f3855589eb
Paulo Ricardo Stradioti <paulo.stradioti@ibm.com>
2022-09-30 17:23:09 -0300

    Adicionando Substituição de Dependências

---
+++ AdaBank.IntegrationTests/CustomFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AdaBank.IntegrationTests
+{
+    public class CustomFactory : IClassFixture<WebApplicationFactory<Api.Program>>
+    {
+        public CustomFactory(WebApplicationFactory<Api.Program> factory)
+        {
+            var client = factory.WithWebHostBuilder(builder =>
+            {
+
+                builder.ConfigureTestServices(services => 
+                {
+                    //services.RemoveAll<IHostedService>();
+                    //services.AddTransient<IHostedService, HostedService>();
+                });
+            })
+            .CreateClient();
+        }
+    }
+}
